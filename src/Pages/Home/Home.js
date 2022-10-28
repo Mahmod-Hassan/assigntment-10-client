b947f5d6f0e73e9d873558efc0bd5dc7a5b703c7
Mahmod Hassan <mahmodhasan7788@gmail.com>
2022-10-28 22:44:45 +0600

    delete old background image url

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,14 +4,6 @@ import Courses from '../Courses/Courses';
 
 const Home = () => {
     return (
-        // <div>
-        //     <Image
-        //         src='https://www.goodcore.co.uk/blog/wp-content/uploads/2019/08/coding-vs-programming-2.jpg'
-        //         className='img-fluid'
-        //     >
-        //     </Image>
-        //     <Courses></Courses>
-        // </div>
         <div>
             <div className='background-image d-flex justify-content-center align-items-center'>
                 <h2 className='text-info fs-1 text-center first-letter'>Learn Programming<br />
